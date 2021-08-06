0874d394fb1b51e193e983690f0db512b6dff409
zooioioq <zooi.oi.oq@gmail.com>
2021-08-06 23:55:47 +0900

    gallery slide banner 설정

--- js/src/index.js
+++ js/src/index.js
@@ -2,12 +2,56 @@
 
 
 // nav viewBox 영역에서는 background-color 없도록
-// 최상단에서 scroll시 100vh 이동
-// gallery slide banner 
 
 
 (function($){
   
+  // 최상단에서 scroll시 100vh 이동
+  var win = $(window);
+  var viewDoc = $('html,body');
+  var viewBox = $('#viewBox');
+  var recommend = true;
+  var countScroll = 0;
+  var winH;
+
+  var setWinHFn = function(){
+    winH = win.outerHeight();
+    viewBox.css({height:winH + 'px'});
+  };
+  
+  var setScrollFn = function(n){
+    recommend = false;
+    var moveH = winH * n;
+    viewDoc.animate({scrollTop:moveH + 'px'}, function(){
+      recommend = true;
+    });
+  };
+
+  setWinHFn();
+  setScrollFn(0);
+
+  viewDoc.on('mousewheel DOMMouseScroll', function(e){
+    var winScrollTop = win.scrollTop();
+                           
+    var evt = e.originalEvent;
+    var delta = 0;
+    (e.type === 'DOMMouseScroll') ? delta = evt.detail * -40 :  delta = evt.wheelDelta;
+    
+    if(recommend){
+      if(delta < 0 && winScrollTop >= 0 && countScroll <= 0){
+        countScroll += 1;
+        setScrollFn(countScroll);
+      }else if(winScrollTop <= winH && delta > 0 && countScroll > 0){
+        countScroll = 0;
+        setScrollFn(countScroll);
+      }
+    }
+  
+  });
+
+
+
+
   
   // about me box 내부 skills tab menu 
   var aboutme = $('#aboutMeBox');
@@ -28,6 +72,59 @@
     skillsDetailUl.eq(_this).siblings().hide();
     skillsDetailUl.eq(_this).show();
   });
+
+
+
+
+
+  // gallery slide banner 
+  var gallery = $('#galleryBox');
+  var galImgArea = gallery.find('.gallery_img_area');
+  var galImgUl = galImgArea.children('ul');
+  var galLiLen = galImgUl.children('li').length;
+  var galBtn = gallery.find('.gallery_btn');
+  var galPrevBtn = galBtn.find('.prev');
+  var galNextBtn = galBtn.find('.next');
+  var permission = true;
+  var i = 0;
+
+  var cloneLiLast = galImgUl.children('li').eq(-1).clone();
+  var cloneLiFirst = galImgUl.children('li').eq(0).clone();
+  galImgUl.prepend(cloneLiLast);
+  galImgUl.append(cloneLiFirst);
+
+  galNextBtn.on('click', function(e){
+    e.preventDefault();
+    if(permission){
+      permission = false;
+      i += 1;
+      if(i > galLiLen-1){
+        i = 0;
+        galImgUl.stop().css({marginLeft : 750 +'px'});
+      }
+      galImgUl.stop().animate({marginLeft : -(750 * i) + 'px'},function(){
+        permission = true;
+      })
+    }
+  });
+
+  galPrevBtn.on('click', function(e){
+    e.preventDefault();
+    if(permission){
+      permission = false;
+      i -= 1;
+      galImgUl.stop().animate({marginLeft : -(750 * i) + 'px'},function(){
+        if(i < 0){
+          i = galLiLen -1;
+          galImgUl.stop().css({marginLeft : -(750 * i) + 'px'});
+        }
+        permission = true;
+      })
+    }
+  });
+
+
+
 
 
 })(jQuery);
